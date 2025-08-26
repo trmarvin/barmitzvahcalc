ee4106bde2aacc4ca86de2a3b2e5aa7ab452a699
Tamar Ron Marvin <trmarvin@gmail.com>
2025-08-26 22:14:53 +0300

    working on api output for hebrew_date

--- bmcalc_main.py
+++ bmcalc_main.py
@@ -1,8 +1,5 @@
 from datetime import datetime, date
-
-# from date_convert import hebrew_date
-# from parsha_find import parsha_calc
-# from pull_info import parsha_info
+import requests
 
 GREGORIAN_START = date(1582, 10, 15)
 
@@ -20,34 +17,53 @@ while True:
     choice = input("Enter your choice: ").strip().upper()
 
     if choice == "A":
+        # --- input validation loop ---
         while True:
-            s = input("Please enter your birthdate (YYYY-MM-DD): ").strip()
+            birthdate = input("Please enter your birthdate (YYYY-MM-DD): ").strip()
             try:
-                birthdate = datetime.strptime(s, "%Y-%m-%d").date()
+                greg_date = datetime.strptime(birthdate, "%Y-%m-%d").date()
             except ValueError as e:
                 print(f"Invalid date: {e}. Use YYYY-MM-DD.")
                 continue
 
-            if birthdate < GREGORIAN_START:
+            if greg_date < GREGORIAN_START:
                 print("Please use a date on/after 1582-10-15 (Gregorian reform).")
                 continue
-            if birthdate > date.today():
+            if greg_date > date.today():
                 print("Birthdate cannot be in the future.")
                 continue
 
-            print("Valid date:", birthdate)
-            break  
+            print("Valid date:", greg_date)
+            break
+
+        url = "https://www.torahcalc.com/api/dateconverter/gregtoheb"
+        params = {
+            "year": greg_date.year,
+            "month": greg_date.month,
+            "day": greg_date.day,
+            "afterSunset": "false",
+        }
+
+        hebrew_date = {
+            'success': True,
+            'data': {
+            'year': 5738,
+            'month': 3,
+            'day': 18,
+            'monthName': 'Sivan',
+            'displayEn': '18th of Sivan, 5738',
+            'displayHe': '18 סִיוָן, 5738',
+            'displayGematriya': 'י״ח סִיוָן תשל״ח'
+            }
+        }
+
+        data = hebrew_date["data"]
+
+        print("Your Hebrew birthday is:", data["displayEn"])
+        print("Hebrew:", data["displayGematriya"])
 
     elif choice == "B":
         print("Enjoy your special parsha! Goodbye!")
         break
-
     else:
         print("Invalid choice. Please try again.")
-
-
-# Display the results
-# print(hebrew_date)
-# print(parsha_calc)
-# print(parsha_info)
-print("Please note, these results are only valid for birthdates after the time of the Gregorian calendar reform in 1582, and later in some locales.")
